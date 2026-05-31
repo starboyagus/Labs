9b042302d58564a992407e8c83712689a6623252
Agus Gil <agussv360@gmail.com>
2026-05-31 18:50:15 -0300

    Parcial echo

---
+++ Parciales/Celular/Celular.Dominio/Celular.cs
@@ -0,0 +1,17 @@
+﻿namespace Celular.Dominio { 
+    public class Celular : Dispositivo
+    {
+        public string Modelo { get; set; }
+
+        public Celular(string nroSerie, string marca, int anioFabricacion, string modelo) : base(nroSerie, marca, anioFabricacion)
+        {
+            Modelo = modelo;
+        }
+
+    public override string ToString()
+    {
+        return $"Serie: {NroSerie} Marca: {Marca} Fabricación: {AnioFabricacion} Model: {Modelo}";
+    }
+
+}
+}
---
+++ Parciales/Celular/Celular.Dominio/Dispositivo.cs
@@ -0,0 +1,19 @@
+﻿namespace Celular.Dominio { 
+    public abstract class Dispositivo
+    {
+        public string NroSerie { get; set; }
+        public string Marca { get; set; }
+        public int AnioFabricacion { get; set; }
+        protected Dispositivo(string nroSerie, string marca, int anioFabricacion)
+        {
+            this.NroSerie = nroSerie;
+            this.Marca = marca;
+            this.AnioFabricacion = anioFabricacion;
+        }
+        public override string ToString()
+        {
+            return $"Serie: {NroSerie} Marca: {Marca} Fabricación: {AnioFabricacion}";
+        }
+    }
+}
+
