235b3ee8b66e07ae2999d44578abe0f08ff924d7
Agus Gil <agussv360@gmail.com>
2026-05-31 19:08:46 -0300

    auto parcial no hecho porque nicolas es dgay

---
+++ Parciales/Auto/Auto.Dominio/ListaVehiculo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auto.Dominio
+{
+    public class ListaVehiculo
+    {
+        public static Vehiculo BuscarPatenteLinq(List<Vehiculo> lista, string patente)
+        {
+            var MiVehiculo = (from v in lista where v.Patente == patente select v).FirstOrDefault();
+
+            return MiVehiculo;
+        }
+
+        public static Vehiculo BuscarPatenteIterativo(List<Vehiculo> lista, string patente)
+        {
+            foreach (var item in lista)
+            {
+                if (item.Patente == patente)
+                {
+                    return item;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ Parciales/Auto/Auto.Dominio/Vehiculo.cs
@@ -0,0 +1,19 @@
+﻿namespace Auto.Dominio
+{
+    public abstract class Vehiculo
+    {
+        public string Patente { get; set; }
+        public int Ruedas { get; set; }
+        public int Modelo { get; set; }
+        protected Vehiculo(string patente, int ruedas, int modelo)
+        {
+            this.Patente = patente;
+            this.Ruedas = ruedas;
+            this.Modelo = modelo;
+        }
+        public override string ToString()
+        {
+            return $"Patente: {Patente} Ruedas: {Ruedas} Modelo: {Modelo}";
+        }
+    }
+}
