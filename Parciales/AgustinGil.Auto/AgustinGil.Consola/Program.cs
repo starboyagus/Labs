6673f3091ae9c6b2f5071dd7236254b90cea9d79
Agus Gil <agussv360@gmail.com>
2026-06-08 12:13:05 -0300

    parcial 2

---
+++ Parciales/AgustinGil.Auto/AgustinGil.Consola/Program.cs
@@ -0,0 +1,13 @@
+﻿using AgustinGil.Dominio;
+
+List<Vehiculo> vehiculos = new List<Vehiculo>();
+vehiculos.Add(new Auto("ABC111", 4, 1999, "Azul"));
+vehiculos.Add(new Auto("ABC123", 4, 1999, "Verde"));
+vehiculos.Add(new Auto("ABC143", 4, 1999, "Rojo"));
+
+Vehiculo BusquedaLinq = ListaVehiculo.BuscarPatenteLINQ(vehiculos, "ABC111");
+Vehiculo BusquedaIT = ListaVehiculo.BuscarPatenteIterativa(vehiculos, "ABC123");
+Console.WriteLine("Busqueda LINQ: ");
+Console.WriteLine(BusquedaLinq);
+Console.WriteLine("Busqueda Iterativa: ");
+Console.WriteLine(BusquedaIT);
---
+++ Parciales/AgustinGil.Auto/AgustinGil.Dominio/ListaVehiculo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgustinGil.Dominio
+{
+    public class ListaVehiculo
+    {
+        public static Vehiculo BuscarPatenteLINQ(List<Vehiculo> vehiculos, string patente)
+        {
+            return vehiculos.Where(v => v.Patente == patente).FirstOrDefault();
+        }
+
+        public static Vehiculo BuscarPatenteIterativa(List<Vehiculo> vehiculos, string patente)
+        {
+            foreach (var vehiculo in vehiculos)
+            {
+                if (vehiculo.Patente == patente)
+                {
+                    return vehiculo;
+                }
+            }
+            return null;
+        }
+    }
+}
